f6c1cb7512d57f3ac5d725b24e196fbfd4c7f07e
lucasdsmachado <briandsmachado@gmail.com>
2023-02-16 18:08:39 -0300

    feat: create post component and its styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Header } from "./components/Header"
-import './global.css'
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css';
+import './global.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://www.github.com/lucasdsmachado.png" alt="" />
+          <div className={styles.authorInfo}>
+            <strong>Brian Machado</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13" dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
+        <p>
+          <a href=''>#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
